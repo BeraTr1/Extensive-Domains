2c2f486937afb139b903da2b80d1b1e014d6862f
BeraTr1 <beratr.alias@gmail.com>
2024-02-16 00:34:40 -0700

    - removed debug souts

--- src/main/java/org/saulo/extensivedomains/managers/DomainTierManager.java
+++ src/main/java/org/saulo/extensivedomains/managers/DomainTierManager.java
@@ -39,7 +39,6 @@ public class DomainTierManager {
             int domainTierLevel = configManager.getConfigInteger(domainTierConfig, key + ".tier_level");
             int populationRequirement = configManager.getConfigInteger(domainTierConfig, key + ".population_req");
             List<String> allowedActionsString = configManager.getListString(domainTierConfig, key + ".allowed_actions");
-            System.out.println("Converting list of string allowed actions to set of allowed actions for tier " + domainTierLevel + "...");
             Set<DomainAction> allowedActions = convertAllowedActionsToSet(allowedActionsString);
             this.tierAllowedActionsMap.put(domainTierLevel, allowedActions);
 
@@ -52,65 +51,41 @@ public class DomainTierManager {
 
     private Set<DomainAction> convertAllowedActionsToSet(List<String> allowedActionsString) {
         Set<DomainAction> allowedActions = new HashSet<>();
-        System.out.println("\tTotal allowed actions to convert: " + allowedActionsString.size());
 
         for (String allowedActionString : allowedActionsString) {
             DomainAction domainAction = DomainAction.getDomainActionFromLabel(allowedActionString);
 
             if (domainAction == null) {
-                System.out.println("\t\tAction '" + allowedActionString + "' not found!");
                 continue;
             }
 
-            System.out.println("\t\tAction '\" + allowedActionString + \"' found, adding it to set...");
 
             allowedActions.add(domainAction);
         }
 
-        System.out.println("\tFinished converting. Total allowed actions in set: " + allowedActions.size());
-        System.out.println("");
         return allowedActions;
     }
 
     private void resolveAllowedActionsInheritance() {
         int registeredTiersAmount = this.tierAllowedActionsMap.size();
 
-        System.out.println("Resolving inheritance of " + registeredTiersAmount + " domain tiers!");
         for (int i = 1; i <= registeredTiersAmount; i++) {
-            System.out.println("\tCurrent tier: " + i);
             int previousTierLevel = i - 1;
             boolean previousTierExists = this.tierAllowedActionsMap.getOrDefault(previousTierLevel, null) != null;
 
             if (!previousTierExists) {
-                System.out.println("\t\tSkipping tier... No tiers to inherit allowed actions from!");
-                System.out.println("");
                 continue;
             }
 
-            System.out.println("\tPrevious tier: " + previousTierLevel);
             Set<DomainAction> previousTierAllowedActions = this.tierAllowedActionsMap.get(previousTierLevel);
             Set<DomainAction> currentTierAllowedActions = this.tierAllowedActionsMap.get(i);
-            System.out.println("\tAllowed actions of CURRENT tier:");
-            for (DomainAction action : currentTierAllowedActions) {
-                System.out.println("\t\t- " + action.getLabel());
-            }
-            System.out.println("\tAllowed actions of PREVIOUS tier:");
-            for (DomainAction action : previousTierAllowedActions) {
-                System.out.println("\t\t- " + action.getLabel());
-            }
             currentTierAllowedActions.addAll(previousTierAllowedActions);
             this.tierAllowedActionsMap.put(i, currentTierAllowedActions);
-            System.out.println("\tUpdated allowed actions of current tier:");
-            for (DomainAction action : this.tierAllowedActionsMap.get(i)) {
-                System.out.println("\t\t- " + action.getLabel());
-            }
-            System.out.println("");
         }
     }
 
     public boolean domainTierAllowsAction(int domainTier, DomainAction action) {
         Set<DomainAction> allowedActions = this.tierAllowedActionsMap.get(domainTier);
-        System.out.println("Domain tier level " + domainTier + " can perform " + action.getLabel() + ": " + allowedActions.contains(action));
         return allowedActions.contains(action);
     }
 
